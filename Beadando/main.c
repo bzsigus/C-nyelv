1a6fe8deb8aafd41fd6584e96cebfcac107920ac
Zsigus Bence <118625873+bzsigus@users.noreply.github.com>
2023-12-19 21:46:45 +0100

    kezdo fileok

---
+++ Beadando/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.c"
+
+
+int main(int *argc, char *argv){
+
+
+
+
+
+
+}
---
+++ Beadando/main.h
@@ -0,0 +1,9 @@
+#ifndef Menu_H
+#define Menu_H
+void menu(void);
+
+
+
+
+
+#endif
