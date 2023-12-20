339fe71c6084a5f6c9b526d9cfaa469e3da0f314
bzsigus <bencezsigus12@gmail.com>
2023-12-20 16:58:46 +0100

    Menu kiiras kész

--- Beadando/main.c
+++ Beadando/main.c
@@ -2,18 +2,10 @@
 #include <stdlib.h>
 #include "menu.c"
 
-void displayMenu() {
-    printf("1. Option 1\n");
-    printf("2. Option 2\n");
-    printf("3. Option 3\n");
-    printf("4. Exit\n");
-}
+void displayMenu(void);
 
 int main(int *argc, char *argv){
-
-
     int choice;
-
     do {
         displayMenu();
         printf("Enter your choice: ");
@@ -33,18 +25,26 @@ int main(int *argc, char *argv){
                 // Add your code for Option 3 here
                 break;
             case 4:
-                printf("Exiting the program.\n");
+                printf("Option 4.\n");
                 break;
+            case 5:
+                return 0;
             default:
                 printf("Invalid choice. Please enter a valid option.\n");
         }
 
-    } while (choice != 4);
+    } while (choice != 5);
 
-    return 0;
+}
 
-
-
-
-
+void displayMenu(void) {
+    printf(" ---------------------\n");
+    printf("|Welcome in the Matrix|\n");
+    printf(" --------------------- \n");
+    printf("   Your Options:\n");
+    printf("    1 - User Manual\n");
+    printf("    2 - Generate matrix\n");
+    printf("    3 - Load Mátrix:\n");
+    printf("    4 - Save Matrix:\n");
+    printf("    5 - exit\n");
 }
